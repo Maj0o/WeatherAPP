38c4a7a14850b0af08be8029a5c1edd88f176406
Quinten <qdderuiter@protonmail.com>
2022-03-11 12:39:40 +0100

    Destructured API, added variable to test input

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ let cityName = `Texas`;
 const API_KEY = `appid=064f8543c0055ed385d8ed5634c95ac9`;
 const CITY_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&${API_KEY}`;
 
-function geoCoordinates() {
-  const dataPromise = fetch(`${CITY_URL}`)
-    .then((response) => response.json())
-    .then((geoData) => console.table(geoData));
-}
+const retrieveData = fetch(`${CITY_URL}`);
+retrieveData.then((response) => response.json());
+retrieveData.then((geoData) => console.log(geoData));
 
+
+console.log(retrieveData)
 // function resolveGeoCoordinates() {
 
 //   api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&${API_KEY}
 // }
-
-geoCoordinates();
