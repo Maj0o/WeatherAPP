14c086bbddb9ccb8fdeb4789093160ee59f3d5e1
Quinten <qdderuiter@protonmail.com>
2022-03-14 11:43:40 +0100

    Rewrote functions using async/await

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const mainDiv = document.getElementById("root");
 
-let cityName = `Texas`;
+let cityName = `Ottawa`;
 const API_KEY = `appid=064f8543c0055ed385d8ed5634c95ac9`;
 const CITY_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&${API_KEY}`;
 
-const retrieveData = fetch(`${CITY_URL}`);
-retrieveData.then((response) => response.json());
-retrieveData.then((geoData) => console.log(geoData));
+const retrieveData = async () => {
+  try {
+    const fetchPromise = await fetch(`${CITY_URL}`);
+    const weatherData = await fetchPromise.json();
+    console.log(weatherData);
+  } catch (error) {
+    console.error(`Could not retrieve data ${error}`);
+  }
+};
 
+retrieveData();
 
-console.log(retrieveData)
-// function resolveGeoCoordinates() {
-
-//   api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&${API_KEY}
-// }
