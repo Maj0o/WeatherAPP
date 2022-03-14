20905e6deb2a380fe0efe2ce6b94dcf143cb17ab
Quinten <qdderuiter@protonmail.com>
2022-03-14 14:47:56 +0100

    Added forEach, JSON.stringify

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const mainDiv = document.getElementById("root");
 
-let cityName = `Ottawa`;
-const API_KEY = `appid=064f8543c0055ed385d8ed5634c95ac9`;
-const CITY_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&${API_KEY}`;
+let cityName = `Groningen`;
 
-const retrieveData = async () => {
-  try {
-    const fetchPromise = await fetch(`${CITY_URL}`);
-    const weatherData = await fetchPromise.json();
-    console.log(weatherData);
-  } catch (error) {
-    console.error(`Could not retrieve data ${error}`);
-  }
+const API_KEY = `064f8543c0055ed385d8ed5634c95ac9`;
+// const GET_WEATHER = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+const CITY_COORDINATES = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`;
+
+const fetchData = async () => {
+  const fetchCityCoordinates = await fetch(`${CITY_COORDINATES}`);
+  const resolveLocation = await fetchCityCoordinates.json();
+  resolveLocation.forEach((data) => {
+    JSON.stringify(`${data.name} ${data.state} ${data.lon} ${data.lat}`);
+  });
+  console.table(resolveLocation);
 };
 
-retrieveData();
-
+fetchData();
