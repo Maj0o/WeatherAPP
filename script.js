2e91b390d8881f4db69fc70c50d79685b3c7cb20
Quinten <qdderuiter@protonmail.com>
2022-03-15 09:53:53 +0100

    reworked fetchData function

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const mainDiv = document.getElementById("root");
 
-let cityName = `Groningen`;
+let cityName = `Texas`;
 
 const API_KEY = `064f8543c0055ed385d8ed5634c95ac9`;
 // const GET_WEATHER = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 const CITY_COORDINATES = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`;
 
 const fetchData = async () => {
-  const fetchCityCoordinates = await fetch(`${CITY_COORDINATES}`);
-  const resolveLocation = await fetchCityCoordinates.json();
-  resolveLocation.forEach((data) => {
-    JSON.stringify(`${data.name} ${data.state} ${data.lon} ${data.lat}`);
-  });
-  console.table(resolveLocation);
+  const fetchPromise = await fetch(`${CITY_COORDINATES}`);
+  const resolveData = await fetchPromise.json();
+  return resolveData;
 };
+fetchData().then(console.log);
 
 fetchData();
